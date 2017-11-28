f3fb99cc04f1792695aaa30a0079b2e5412b06fb
Francisco do Vale <francisco.vale@ctis.com.br>
2017-11-28 11:19:04 -0300

    Implementação da ordenação de jogos

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 import { GamesService } from './games.service';
 
@@ -9,16 +9,17 @@ import { Game } from './game.model';
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.scss']
 })
-export class GamesComponent implements OnInit {
+export class GamesComponent implements OnInit  {
 
   filterOptions: any[];
-  games: Game;
+  games: any;
+  gameFilter: any;
 
   constructor(private gService: GamesService) { }
 
   ngOnInit() {
-
     this.filterOptions = this.gService.getFilterOptions();
+    this.gameFilter = this.filterOptions[0];
 
     this.gService
       .getGames()
@@ -26,8 +27,30 @@ export class GamesComponent implements OnInit {
         res => this.games = res,
         err => console.log(err)
       );
+  }
 
+  getSelectedValue() {
+    this.filterGames(this.gameFilter)
+  }
+
+  filterGames(filterOption: any) {
+    switch (parseInt(filterOption)) {
+      case 0:
+        this.games = this.games.sort((a, b) => a.price > b.price);
+        break;
+      case 1:
+        this.games = this.games.sort((a, b) => a.score > b.score);
+        break;
+      case 2:
+        this.games = this.games.sort((a, b) => a.name > b.name);
+        break;
+      default:
+        console.log('Opção inválida');
+        break;
+    }
 
   }
+
+
 
 }
